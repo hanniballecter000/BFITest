83508ec2434ad5cc9a30137aea452aec02855f53
hanniballecter000 <43736713+hanniballecter000@users.noreply.github.com>
2018-10-01 21:15:25 +0700

    Add files via upload

---
+++ Soal2/Soal2/Soal2/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Soal2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenRand = new System.Windows.Forms.Button();
+            this.txtHasil = new System.Windows.Forms.TextBox();
+            this.LabelTgl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtJumlah = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGenRand
+            // 
+            this.btnGenRand.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenRand.Location = new System.Drawing.Point(180, 20);
+            this.btnGenRand.Name = "btnGenRand";
+            this.btnGenRand.Size = new System.Drawing.Size(75, 41);
+            this.btnGenRand.TabIndex = 0;
+            this.btnGenRand.Text = "Generate Random";
+            this.btnGenRand.UseVisualStyleBackColor = true;
+            this.btnGenRand.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtHasil
+            // 
+            this.txtHasil.Location = new System.Drawing.Point(18, 67);
+            this.txtHasil.Multiline = true;
+            this.txtHasil.Name = "txtHasil";
+            this.txtHasil.Size = new System.Drawing.Size(237, 182);
+            this.txtHasil.TabIndex = 1;
+            // 
+            // LabelTgl
+            // 
+            this.LabelTgl.AutoSize = true;
+            this.LabelTgl.Location = new System.Drawing.Point(15, 48);
+            this.LabelTgl.Name = "LabelTgl";
+            this.LabelTgl.Size = new System.Drawing.Size(88, 13);
+            this.LabelTgl.TabIndex = 2;
+            this.LabelTgl.Text = "Tanggal : ,Bulan:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Jumlah random :";
+            // 
+            // txtJumlah
+            // 
+            this.txtJumlah.Location = new System.Drawing.Point(109, 18);
+            this.txtJumlah.MaxLength = 2;
+            this.txtJumlah.Name = "txtJumlah";
+            this.txtJumlah.Size = new System.Drawing.Size(43, 20);
+            this.txtJumlah.TabIndex = 4;
+            this.txtJumlah.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtJumlah_KeyPress);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtJumlah);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LabelTgl);
+            this.Controls.Add(this.txtHasil);
+            this.Controls.Add(this.btnGenRand);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Generate Random";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenRand;
+        private System.Windows.Forms.TextBox txtHasil;
+        private System.Windows.Forms.Label LabelTgl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtJumlah;
+    }
+}
+
---
+++ Soal2/Soal2/Soal2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soal2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            LabelTgl.Text = "Tanggal : " + DateTime.Now.ToString("dd") +", Bulan : " + DateTime.Now.ToString("MM");
+
+            txtHasil.Text = GenerateManyRandom(Convert.ToInt32(txtJumlah.Text.ToString()));
+
+        }
+            
+
+        public string GenerateManyRandom(int count)
+        {
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < count; i++)
+            {
+                builder.Append(GenerateRandom() + Environment.NewLine);
+            }  
+
+
+
+
+            return builder.ToString();
+        }
+        // Generate a random password  
+        public string GenerateRandom()
+        {
+
+
+            StringBuilder builder = new StringBuilder();
+            // 16 karakter (A-Z,a-z,0-9)
+            // karakter 1-7 bebas 
+            builder.Append(CreateRandomCharacter(7));
+            // karakter 8 & 9 merupakan tanggal program berjalan
+            builder.Append(DateTime.Now.ToString("dd"));
+            // karakter 10-14 bebas 
+            builder.Append(CreateRandomCharacter(5));
+            // karakter 15 & 16 merupakan Bulan program berjalan
+            builder.Append(DateTime.Now.ToString("MM"));
+            
+            
+            return builder.ToString();
+        }
+
+
+  
+        public  string CreateRandomCharacter(int maxSize)
+        {
+            char[] chars = new char[62];
+            chars =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[1];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetNonZeroBytes(data);
+                data = new byte[maxSize];
+                crypto.GetNonZeroBytes(data);
+            }
+            StringBuilder result = new StringBuilder(maxSize);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return result.ToString();
+        }
+
+        private void txtJumlah_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+            (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
