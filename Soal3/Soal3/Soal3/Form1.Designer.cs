3bd679afbabbd21bc6a6edbe0d5e8d3a32a05141
hanniballecter000 <43736713+hanniballecter000@users.noreply.github.com>
2018-10-01 21:15:51 +0700

    Add files via upload

---
+++ Soal3/Soal3/Soal3/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Soal3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 58);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(421, 172);
+            this.txtOutput.TabIndex = 7;
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(186, 14);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(57, 26);
+            this.btnOutput.TabIndex = 6;
+            this.btnOutput.Text = "Output";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(59, 18);
+            this.txtInput.MaxLength = 4;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 5;
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Input :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 258);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Soal 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Soal3/Soal3/Soal3/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soal3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            
+            if (Convert.ToInt32(txtInput.Text) > 1000 || Convert.ToInt32(txtInput.Text) < 1)
+            {
+                MessageBox.Show("Input 1-1000", "Validasi");   
+                return;
+            }
+
+            int Jumlah = Convert.ToInt32(txtInput.Text);
+            string HasilOutput = "";
+
+            for (int i = 1; i <= Jumlah; i++)
+            {
+
+                int Mod3 = i % 3;
+                int Mod5 = i % 5;
+                int Mod7 = i % 7;
+                bool Check = false;
+
+                if (Mod3 == 0)
+                {
+                    HasilOutput += "X";
+                    Check = true;
+                }
+                if (Mod5 == 0)
+                {
+                    HasilOutput += "Y";
+                    Check = true;
+                }
+
+                if (Mod7 == 0)
+                {
+                    HasilOutput += "Z";
+                    Check = true;
+                }
+
+                if (Check == false)
+                {
+                    HasilOutput += i.ToString() + " ";
+                }
+                else
+                { HasilOutput += " "; }
+
+
+            }
+
+            txtOutput.Text = HasilOutput;
+        }
+
+        private void txtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+            (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
