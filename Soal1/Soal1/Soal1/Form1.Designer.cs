62bd786f8f184a9f656ccc807e7a986d4d660916
hanniballecter000 <43736713+hanniballecter000@users.noreply.github.com>
2018-10-01 21:14:34 +0700

    Add files via upload

---
+++ Soal1/Soal1/Soal1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Soal1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input :";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(57, 24);
+            this.txtInput.MaxLength = 20;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(205, 20);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(282, 24);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(57, 26);
+            this.btnOutput.TabIndex = 2;
+            this.btnOutput.Text = "Output";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(10, 64);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(421, 172);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(354, 24);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(57, 26);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 248);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Soal 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Soal1/Soal1/Soal1/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Soal1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public class Item
+        {
+            public string country;
+            public string name;
+            public decimal lat;
+            public Decimal lng;
+            
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+           
+            using (StreamReader r = new StreamReader("cities.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+
+                items = items.Where(x => x.country.Equals("ID")).ToList();
+
+                string input = txtInput.Text;
+                int hasil;
+                string hasiloutput = "";
+
+                for (var i = 0; i < items.Count; i++)
+                {
+                                                          
+                    hasil = StringDistance.LevenshteinDistance(input.ToLower(), items[i].name.ToLower());
+
+                    decimal persenPerubahan = 0;
+
+                 
+
+                    persenPerubahan = ((decimal)hasil / (decimal)input.Length) * 100;
+
+                    if (persenPerubahan <= 30)
+                    {
+                        hasiloutput += items[i].name + " (" + persenPerubahan + ")" + " ,";
+                        }
+
+                   
+                    
+
+                  }
+
+
+                txtOutput.Text = hasiloutput;
+
+            }
+
+            MessageBox.Show("Done!");
+          
+        }
+
+        public static class StringDistance
+        {
+
+
+            public static int GetHammingDistance(string s, string t)
+            {
+                if (s.Length != t.Length)
+                {
+                    throw new Exception("Strings must be equal length");
+                }
+
+                int distance =
+                    s.ToCharArray()
+                    .Zip(t.ToCharArray(), (c1, c2) => new { c1, c2 })
+                    .Count(m => m.c1 != m.c2);
+
+                return distance;
+            }
+
+            public static int GetDamerauLevenshteinDistance(string s, string t)
+            {
+                var bounds = new { Height = s.Length + 1, Width = t.Length + 1 };
+
+                int[,] matrix = new int[bounds.Height, bounds.Width];
+
+                for (int height = 0; height < bounds.Height; height++) { matrix[height, 0] = height; };
+                for (int width = 0; width < bounds.Width; width++) { matrix[0, width] = width; };
+
+                for (int height = 1; height < bounds.Height; height++)
+                {
+                    for (int width = 1; width < bounds.Width; width++)
+                    {
+                        int cost = (s[height - 1] == t[width - 1]) ? 0 : 1;
+                        int insertion = matrix[height, width - 1] + 1;
+                        int deletion = matrix[height - 1, width] + 1;
+                        int substitution = matrix[height - 1, width - 1] + cost;
+
+                        int distance = Math.Min(insertion, Math.Min(deletion, substitution));
+
+                        if (height > 1 && width > 1 && s[height - 1] == t[width - 2] && s[height - 2] == t[width - 1])
+                        {
+                            distance = Math.Min(distance, matrix[height - 2, width - 2] + cost);
+                        }
+
+                        matrix[height, width] = distance;
+                    }
+                }
+
+                return matrix[bounds.Height - 1, bounds.Width - 1];
+            }
+
+            public static int LevenshteinDistance(string s, string t)
+            {
+                int n = s.Length;
+                int m = t.Length;
+                int[,] d = new int[n + 1, m + 1];
+
+                // Step 1
+                if (n == 0)
+                {
+                    return m;
+                }
+
+                if (m == 0)
+                {
+                    return n;
+                }
+
+                // Step 2
+                for (int i = 0; i <= n; d[i, 0] = i++)
+                {
+                }
+
+                for (int j = 0; j <= m; d[0, j] = j++)
+                {
+                }
+
+                // Step 3
+                for (int i = 1; i <= n; i++)
+                {
+                    //Step 4
+                    for (int j = 1; j <= m; j++)
+                    {
+                        // Step 5
+                        int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                        // Step 6
+                        d[i, j] = Math.Min(
+                            Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                            d[i - 1, j - 1] + cost);
+                    }
+                }
+                // Step 7
+                return d[n, m];
+            }
+
+        }
+
+        private void txtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+            txtOutput.Text = "";
+            
+        }
+    }
+}
